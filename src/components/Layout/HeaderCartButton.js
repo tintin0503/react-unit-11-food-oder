45a8630ac666c4202f3308f67ef75d96ced1a2de
quanbv <quanbv@reactplus.jp>
2022-12-14 17:04:08 +0700

    add modal use portal

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -5,7 +5,7 @@ import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
 
-  return <button className={classes.button}>
+  return <button className={classes.button} onClick={props.onClick}>
     <span className={classes.icon}>
       <CartIcon />
     </span>
