b743c5e1a56f8bec8beb810e2da278ae687c88fa
quanbv <quanbv@reactplus.jp>
2022-12-14 14:36:17 +0700

    add meal item form

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import classes from './MealItem.module.css';
+import MealItemForm from "./MealItemForm";
 
 const MealItem = props => {
 
@@ -14,7 +15,7 @@ const MealItem = props => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-
+        <MealItemForm />
       </div>
     </li>
   </>
